063870e112b4b1fa35ec7fef690dadd4410be403
Owen <owenyangg@gmail.com>
2017-05-13 13:47:07 +1000

    mocks up dummy transaction data

--- src/components/transactions.js
+++ src/components/transactions.js
@@ -1,9 +1,47 @@
 import React, { Component } from 'react';
 import GlobalHeader from './globalHeader.js';
 import { Grid, Row, Col } from 'react-bootstrap';
-class Transactions extends Component {
 
+class Transactions extends Component {
   render() {
+    var companies = ["AirTasker", "Uber", "Upwork", "AirBnB", "Deliveroo"]
+    var dates = ["17/05/2017", "16/05/2017", "15/05/2017", "14/05/2017"]
+
+    var transactions = [];
+
+    for (var i = 0; i < 20; i++ ){
+      transactions.push({
+        source: companies[Math.floor(Math.random()*companies.length)],
+        amount: Math.floor(Math.random()*100),
+        date: dates[Math.floor(Math.random()*dates.length)]
+      })
+    }
+
+    var table = transactions.map(function(e){
+      return (
+        <Row className="show-grid">
+            <Col sm={6} md={3} >
+              <p>{e.source}</p>
+            </Col>
+
+            <Col sm={6} md={3}>
+              <p>{e.amount}</p>
+            </Col>
+
+
+            <Col sm={6} md={3}>
+              <p>{e.date}</p>
+            </Col>
+
+            <Col sm={6} md={3}>
+              <a type="button" className="btn btn-info btn-block" href={'#'}>
+                Confirm
+              </a>
+            </Col>
+        </Row>
+      )
+    })
+
     return (
       <div>
         <GlobalHeader />
@@ -26,6 +64,7 @@ class Transactions extends Component {
               <p>CONFIRMATION</p>
             </Col>
           </Row>
+          {table}
        </Grid>
     </div>
   )}
