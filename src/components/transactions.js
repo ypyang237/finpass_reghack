e6c8e981c4a9841d3430bc4c5913f919e4c92d71
Owen <owenyangg@gmail.com>
2017-05-13 12:52:42 +1000

    changed to class

--- src/components/transactions.js
+++ src/components/transactions.js
@@ -1,57 +1,57 @@
 import React, { Component } from 'react';
 import GlobalHeader from './globalHeader.js'
 
-const Transactions = () => (
-  <div>
-    <GlobalHeader />
+class Transactions extends Component {
 
-    <div className="col-lg-3 col-md-4 col-sm-6">
-      <div className={`container-fluid`}>
+  render() {
+    return (
+     <div>
+      <GlobalHeader />
+      <div className="col-lg-3 col-md-4 col-sm-6">
+        <div className={`container-fluid`}>
 
-        <div>
+          <div>
 
-          <div className="row">
-            <div className="col-sm-12">
-              <p>SOURCE</p>
+            <div className="row">
+              <div className="col-sm-12">
+                <p>SOURCE</p>
+              </div>
             </div>
+
+            <div className="row">
+              <div className="col-sm-12">
+                <p>AMOUNT</p>
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-sm-12">
+                <p>DATE</p>
+              </div>
+            </div>
+
+
+            <div className="row">
+              <div className="col-sm-12">
+                <p>CONFIRMATION</p>
+              </div>
+            </div>
+
           </div>
 
           <div className="row">
-            <div className="col-sm-12">
-              <p>AMOUNT</p>
-            </div>
-          </div>
-
-          <div className="row">
-            <div className="col-sm-12">
-              <p>DATE</p>
-            </div>
-          </div>
-
-
-          <div className="row">
-            <div className="col-sm-12">
-              <p>CONFIRMATION</p>
+            <div className={`col-sm-6`}>
+              <a type="button" className="btn btn-info btn-block" href={'#'}>
+                Confirm
+              </a>
             </div>
           </div>
 
         </div>
-
-        <div className="row">
-          <div className={`col-sm-6`}>
-            <a type="button" className="btn btn-info btn-block" href={'#'}>
-              Confirm
-            </a>
-          </div>
-        </div>
-
       </div>
-
-
     </div>
-
-  </div>
-)
+  )}
+}
 
 
 export default Transactions;
